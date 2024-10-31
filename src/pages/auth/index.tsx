3381a0372194225270787b127250a0c2839d531d
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-10-31 17:45:43 +0330

    change login position

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,7 @@
 import { LoginForm } from "./partials";
 export const AuthPage = () => {
   return (
-    <div className=" w-[30em] mt-56  mx-auto  shadow-md  rounded-3xl border border-neutral-400 ">
+    <div className=" w-[30em] max-h-screen my-[25vh] mx-auto  shadow-md  rounded-3xl border border-neutral-400 ">
       <LoginForm />
     </div>
   );
