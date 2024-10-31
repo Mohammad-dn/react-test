9c6c64821d64ec2b006cbfe553ea4468b6458630
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-10-31 17:42:54 +0330

    change margin

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,7 @@
 import { LoginForm } from "./partials";
 export const AuthPage = () => {
   return (
-    <div className=" w-[30em] my-20  mx-auto  shadow-md  rounded-3xl border border-neutral-400 ">
+    <div className=" w-[30em] mt-56  mx-auto  shadow-md  rounded-3xl border border-neutral-400 ">
       <LoginForm />
     </div>
   );
